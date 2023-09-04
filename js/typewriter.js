aea8664a65510ad4bc809296cc2b59883f48c279
Alissa Kruiniger <alissa.kruiniger@gmail.com>
2023-09-04 14:53:25 +0200

    Pressing spacebar skips typewriter effect

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,19 +1,33 @@
-const textElement = document.getElementById('fetch');
+const textElement = document.getElementById("fetch");
 const originalText = textElement.textContent.trim();
-textElement.textContent = '';
+textElement.textContent = "";
 
 let index = 0;
+let typingInProgress = true;
 
 function typeNextCharacter() {
-  textElement.textContent += originalText[index];
-  index++;
+  if (typingInProgress) {
+    textElement.textContent += originalText[index];
+    index++;
 
-  if (index < originalText.length) {
-    setTimeout(typeNextCharacter, 40);
-  } else {
-    const linkContainer = document.getElementById('link-container');
-    linkContainer.classList.add('visible');
+    if (index < originalText.length) {
+      setTimeout(typeNextCharacter, 40);
+    } else {
+      const linkContainer = document.getElementById("link-container");
+      linkContainer.classList.add("visible");
+      typingInProgress = false; // Typing is complete
+    }
   }
 }
 
 typeNextCharacter();
+
+// If space bar is pressed and typing is in progress, skip typing and display content
+document.addEventListener("keydown", function (event) {
+  if (event.key === " " && typingInProgress) {
+    textElement.textContent = originalText;
+    typingInProgress = false;
+    const linkContainer = document.getElementById("link-container");
+    linkContainer.classList.add("visible");
+  }
+});
